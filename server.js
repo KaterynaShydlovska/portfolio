84936c171ac36a3d18de7de355930da8b1a499d0
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-04 08:52:47 -0800

    fixed port

--- server.js
+++ server.js
@@ -5,19 +5,4 @@ const app = express();
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
-
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained'
-  }
-  response.status(200).json(airplanes);
-});
-
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
-
-app.listen(3005, () => console.log('App listening on port 3000!'));
+app.listen(3000, () => console.log('App listening on port 3000!'));
