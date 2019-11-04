f56bdf5c5fb93be3bc14504428fae0bda3ee96cc
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-04 08:31:36 -0800

    Fixed name folder

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./starter-code'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.send('Some random thing');
+  res.send('Test');
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
